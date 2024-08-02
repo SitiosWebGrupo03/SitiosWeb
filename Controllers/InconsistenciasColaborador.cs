867a2d4faa58d27a12d3058c803ccf148a7ad3b5
calde <calde@ALEX>
2024-08-02 16:48:26 -0600

    FAb

--- Controllers/InconsistenciasColaborador.cs
+++ Controllers/InconsistenciasColaborador.cs
@@ -39,29 +39,10 @@ namespace SitiosWeb.Controllers
             return View(inconsistencias);
         }
 
+
+
         public async Task<IActionResult> Justificar()
         {
-            //if (string.IsNullOrEmpty(Request.Cookies["Id"]))
-            //{
-            //    return BadRequest("El ID del colaborador es requerido");
-            //}
-
-            //var colaborador = await _context.Colaboradores
-            //    .FirstOrDefaultAsync(c => c.Identificacion == Request.Cookies["Id"]);
-
-            //if (colaborador == null)
-            //{
-            //    return NotFound("Colaborador no encontrado");
-            //}
-
-            //var inconsistencias = await _context.Inconsistencias
-            //    .Include(i => i.IdEmpleadoNavigation)
-            //    .Include(i => i.IdJustificacionNavigation)
-            //    .Include(i => i.IdTipoInconsistenciaNavigation)
-            //    .Where(i => i.IdEmpleadoNavigation.Identificacion == Request.Cookies["Id"])
-            //    .ToListAsync();
-
-            //return View(inconsistencias);
             var tiposInconsistencias = await _context.TiposInconsistencias
                .Select(ti => new { ti.IdTipoInconsistencia, ti.Descripcion })
                .ToListAsync();
@@ -76,10 +57,7 @@ namespace SitiosWeb.Controllers
                 .Select(ti => new { ti.IdTipoInconsistencia, ti.Descripcion })
                 .ToListAsync();
 
-            ViewBag.TiposInconsistencias = tiposInconsistencias;
-
-            return View("~/Views/InconsistenciasColaborador/JustificarInconsistenciasColaborador.cshtml");
-
+            return Json(tiposInconsistencias);
         }
     }
 }
