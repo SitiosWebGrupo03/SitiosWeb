f39b1124c6175e62d0a2ae5dd6c52a91e3985bd7
Luis Camacho Bonilla <luis.camacho.bonilla@cuc.cr>
2024-08-05 22:46:51 -0600

    R

--- Model/RegistroActividades.cs
+++ Model/RegistroActividades.cs
@@ -1,29 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace SitiosWeb.Model;
-
-public partial class RegistroActividades
+namespace SitiosWeb.Model
 {
-    public int IdRegistro { get; set; }
+    public partial class RegistroActividades
+    {
+        public int IdRegistro { get; set; }
+        public int? IdTipoActividad { get; set; }
+        public string? IdColaborador { get; set; }
+        public string? IdValidador { get; set; }
+        public string Observaciones { get; set; } = null!;
+        public DateOnly FechaActividad { get; set; }
+        public decimal DuracionEnHoras { get; set; }
+        public bool Aprobado { get; set; }
 
-    public int? IdTipoActividad { get; set; }
-
-    public string? IdColaborador { get; set; }
-
-    public string? IdValidador { get; set; }
-
-    public string Observaciones { get; set; } = null!;
-
-    public DateOnly FechaActividad { get; set; }
-
-    public decimal DuracionEnHoras { get; set; }
-
-    public bool Aprobado { get; set; }
-
-    public virtual Colaboradores? IdColaboradorNavigation { get; set; }
-
-    public virtual TipoActividades? IdTipoActividadNavigation { get; set; }
-
-    public virtual Colaboradores? IdValidadorNavigation { get; set; }
+        public virtual Colaboradores? IdColaboradorNavigation { get; set; }
+        public virtual TipoActividades? IdTipoActividadNavigation { get; set; }
+        public virtual Colaboradores? IdValidadorNavigation { get; set; }
+    }
 }
