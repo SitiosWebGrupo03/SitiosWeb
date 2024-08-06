2fc713adafce3f72224ecc1a2b53325646475a9c
calde <calde@ALEX>
2024-08-05 23:11:23 -0600

    fab

--- Controllers/SolicitudeRebajoController.cs
+++ Controllers/SolicitudeRebajoController.cs
@@ -18,6 +18,23 @@ namespace SitiosWeb.Controllers
             _context = context;
         }
 
+        [HttpGet]
+        public async Task<IActionResult> DescargarEvidencia(int id)
+        {
+            var justificacion = await _context.JustificacionesInconsistencias
+                .FirstOrDefaultAsync(j => j.IdJustificacion == id);
+
+            if (justificacion == null || justificacion.Evidencias == null)
+            {
+                return NotFound();
+            }
+
+            var archivoBytes = justificacion.Evidencias;
+            var archivoNombre = $"Evidencia_{justificacion.IdJustificacion}.pdf"; // Ajusta la extensión según el tipo de archivo
+
+            return File(archivoBytes, "application/octet-stream", archivoNombre);
+        }
+
         // GET: SolicitudeRebajo
         public async Task<IActionResult> Index()
         {
@@ -99,110 +116,6 @@ namespace SitiosWeb.Controllers
             await _context.SaveChangesAsync();
 
             return View();
-        }
-
-
-        // GET: SolicitudeRebajo/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("IdSolicitud,IdSolicitante,IdInconsistencia,Observaciones")] SolicitudeRebajo solicitudeRebajo)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(solicitudeRebajo);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(solicitudeRebajo);
-        }
-
-        // GET: SolicitudeRebajo/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var solicitudeRebajo = await _context.SolicitudeRebajo.FindAsync(id);
-            if (solicitudeRebajo == null)
-            {
-                return NotFound();
-            }
-            return View(solicitudeRebajo);
-        }
-
-        // POST: SolicitudeRebajo/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("IdSolicitud,IdSolicitante,IdInconsistencia,Observaciones")] SolicitudeRebajo solicitudeRebajo)
-        {
-            if (id != solicitudeRebajo.IdSolicitud)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(solicitudeRebajo);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!SolicitudeRebajoExists(solicitudeRebajo.IdSolicitud))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(solicitudeRebajo);
-        }
-
-        // GET: SolicitudeRebajo/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var solicitudeRebajo = await _context.SolicitudeRebajo
-                .FirstOrDefaultAsync(m => m.IdSolicitud == id);
-            if (solicitudeRebajo == null)
-            {
-                return NotFound();
-            }
-
-            return View(solicitudeRebajo);
-        }
-
-        // POST: SolicitudeRebajo/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-            var solicitudeRebajo = await _context.SolicitudeRebajo.FindAsync(id);
-            if (solicitudeRebajo != null)
-            {
-                _context.SolicitudeRebajo.Remove(solicitudeRebajo);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         private bool SolicitudeRebajoExists(int id)
